b7a346f4d391436b287e5a30ae917c33bfda4a3b
Alvaro Gabriel Goulart Rodrigues <alvaro.gr@aluno.ifsc.edu.br>
2024-04-19 00:00:56 -0300

    Lista 17/04

---
+++ Desktop.java
@@ -0,0 +1,22 @@
+package atividade;
+
+import java.util.ArrayList;
+
+public class Desktop extends Produtos {
+	
+	private Boolean Gamer;
+	ArrayList <String> pecas = new ArrayList<>();
+	public Boolean getGamer() {
+		return Gamer;
+	}
+	public void setGamer(Boolean gamer) {
+		Gamer = gamer;
+	}
+	public ArrayList<String> getPecas() {
+		return pecas;
+	}
+	public void setPecas(ArrayList<String> pecas) {
+		this.pecas = pecas;
+	}
+	
+}
